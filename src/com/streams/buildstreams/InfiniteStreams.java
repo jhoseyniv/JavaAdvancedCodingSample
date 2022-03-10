9a1abfe4210fc5047549694dd681048b053117f4
jhoseyniv <jhoseyniv@gmail.com>
2022-03-10 12:32:45 +0330

    add InfiniteStreams.java

--- src/com/streams/buildstreams/InfiniteStreams.java
+++ src/com/streams/buildstreams/InfiniteStreams.java
@@ -5,7 +5,21 @@ import java.util.stream.Stream;
 public class InfiniteStreams {
     public static void main(String args[]){
         Stream.iterate(0, n -> n + 5)
-                .limit(10000000)
+                .limit(10)
+                .forEach(System.out::println);
+
+        Stream.iterate(new int[]{0,1} , t -> new int[]{t[1],t[0]+t[1]})
+                .limit(20)
+                .forEach(t -> System.out.println("(" + t[0] + "," + t[1] +")"));
+
+
+        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1],t[0] + t[1]}).limit(10)
+                .map(t -> t[0]).forEach(System.out::println);
+
+
+        //Similarly to the method iterate, the method generate lets you produce an infinite  stream
+        Stream.generate(Math::random)
+                .limit(5)
                 .forEach(System.out::println);
     }
 }
